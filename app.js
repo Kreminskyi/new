ad9928cdae4aa4dfb6fb63b12f1d6bc2016b7b2a
Kreminskyi <kreminsky@bigmir.net>
2017-05-14 13:48:18 +0300

    updated

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
 
-var express = require('express')
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+console.log('Server running at http://127.0.0.1:1337/');
